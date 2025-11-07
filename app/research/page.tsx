444fab427b9c0d7ba45c13dd224788abf53a80a4
daqhris <817573+daqhris@users.noreply.github.com>
2025-11-07 12:37:33 +0100

    ajout de mots

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -64,7 +64,7 @@ export default function ResearchPage() {
               target="_blank"
               rel="noopener noreferrer"
             >Duck.ai</Link>. 
-            Textes originaux des réponses attachés. <br></br><br></br>
+            Textes originaux des réponses du 2e entretien attachés. <br></br><br></br>
             La <strong>1re</strong> question était de définir 3 mots clés et n&apos;a été posée qu&apos;à une créature linguistique.
             La <strong>2e</strong> question a été posée à 5 génies numériques sans corps physiques. Le script publié des mois auparavant ne leur avait pas été donné et leur navigation vers d&apos;autres adresses web était inactive. 
             Une allusion au récit en Anglais a été faite sans fournir une copie du texte. Appuyez sur leurs noms pour afficher les réponses ci-dessous. 
